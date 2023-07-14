b1860a00120df13153fb85e0c0d9d7abbe36292f
gualbertocuiza <gualberto.cuiza@unosquare.com>
2023-07-14 09:40:07 -0400

    Base UI views

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "./components/header.vue";
+import Main from "./components/main.vue";
+</script>
 
 <template>
-  <div>
-    <h2>Here</h2>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <div class="card w-96 bg-base-100 shadow-xl">
-      <figure>
-        <img
-          src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-          alt="Shoes"
-        />
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-          <button class="btn btn-primary">Buy Now</button>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Header />
+  <Main />
 </template>
-
-<style scoped></style>
