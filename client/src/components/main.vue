1ac8d76b93b5e9a964a5fb6ae859534b1c297001
gualbertocuiza <gualberto.cuiza@unosquare.com>
2023-07-19 13:54:20 -0400

    Storing data on localstorage

--- client/src/components/main.vue
+++ client/src/components/main.vue
@@ -3,13 +3,14 @@ import WeatherCard from './weatherCard.vue'
 import SearchInput from './searchInput.vue'
 import { useWeather } from '../composables/useWeather'
 
-const { loading, currentWeather, setCity } = useWeather()
+const { loading, setCity, storageWeathers } = useWeather()
 </script>
 <template>
   <SearchInput @setCity="setCity" />
   <div class="flex justify-center">
     <span v-if="loading" class="loading loading-bars loading-lg"></span>
-
-    <WeatherCard v-else-if="currentWeather" :weather="currentWeather" />
+    <template v-else>
+      <WeatherCard v-for="weather in storageWeathers.data" :weather="weather" />
+    </template>
   </div>
 </template>
--- client/src/composables/useWeather.ts
+++ client/src/composables/useWeather.ts
@@ -1,17 +1,49 @@
 import { Weather } from '../interfaces/Weather'
 import { getCurrentWeather } from '../services/weatherService'
 import errorHandler from '../utils/errorHandler'
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+
+interface storageWeather {
+  data: {
+    [city: string]: Weather
+  }
+}
 
 const loading = ref<boolean>(false)
 const currentWeather = ref<Weather>()
+const storageWeathers = ref<storageWeather>({ data: {} })
 
 export const useWeather = () => {
+  onMounted(() => {
+    getStoragedWeathers()
+  })
+
+  const getStoragedWeathers = () => {
+    let weather = localStorage.getItem('cities')
+    if (weather) {
+      let data = JSON.parse(weather)
+      storageWeathers.value = data
+    }
+  }
+
   const setCity = async (city: string) => {
     loading.value = true
+    let current = storageWeathers.value.data[getCityKey(city)]
+    if (current) {
+      currentWeather.value = current
+      loading.value = false
+      return
+    }
+    return requestWeather(city)
+  }
+
+  const requestWeather = async (city: string) => {
     try {
       const { data } = await getCurrentWeather(city)
-      currentWeather.value = data
+      if (data) {
+        currentWeather.value = data
+        storeWeatherCity(city, data)
+      }
     } catch (error) {
       console.log(error)
       errorHandler(error)
@@ -20,9 +52,19 @@ export const useWeather = () => {
     }
   }
 
+  const storeWeatherCity = (city: string, data: Weather) => {
+    storageWeathers.value.data[getCityKey(city)] = data
+    localStorage.setItem('cities', JSON.stringify(storageWeathers.value))
+  }
+
+  const getCityKey = (city: string) => {
+    return city.replace(',', '').replaceAll(' ', '-')
+  }
+
   return {
     loading,
     currentWeather,
     setCity,
+    storageWeathers,
   }
 }
